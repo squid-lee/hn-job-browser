a8545323d940b8eb84576c724c06a72b147b0cca
Joe Slade <correspondence+hnj@jpslade.com>
2024-06-12 17:23:09 +0100

    Add forwards and backwards keys

--- app/HNI/App.hs
+++ app/HNI/App.hs
@@ -1,10 +1,10 @@
-module HNI.App where
+module HNI.App (newState, app) where
 
 import Brick
 import Data.List.Zipper
 -- import Data.Text
 
-import Graphics.Vty (defAttr)
+import Graphics.Vty (Event (..), Key (..), defAttr)
 import HNI.Post
 import HNI.PrettyPrint
 
@@ -21,10 +21,22 @@ app =
   App
     { appDraw = drawWidget,
       appChooseCursor = showFirstCursor,
-      appHandleEvent = \_ -> return (),
+      appHandleEvent = appEvent,
       appStartEvent = return (),
       appAttrMap = const (attrMap defAttr [])
     }
 
 drawWidget :: State -> [Widget ()]
-drawWidget s = [str . ppPost . head . toList . posts $ s]
+drawWidget s = [str . ppPost . cursor . posts $ s]
+
+appEvent :: BrickEvent () e -> EventM () State ()
+appEvent (VtyEvent e) =
+  case e of
+    EvKey (KChar 'n') [] ->
+      modify $ \s -> s {posts = right $ posts s}
+    EvKey (KChar 'p') [] ->
+      modify $ \s -> s {posts = left $ posts s}
+    EvKey (KChar 'q') [] -> halt
+    EvKey KEsc [] -> halt
+    _ -> return ()
+appEvent _ = return ()
