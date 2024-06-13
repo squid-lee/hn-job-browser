2576d10dec1d4652befbdb9e81a6ea3062415b10
Joe Slade <correspondence+hnj@jpslade.com>
2024-06-13 13:06:13 +0100

    Switch to tdfa - pcre has bugs with unicode

--- app/HNI/Salience.hs
+++ app/HNI/Salience.hs
@@ -7,7 +7,7 @@ import Data.Text (Text, pack, unpack)
 import GHC.Generics
 import HNI.Decoded
 import HNI.Post
-import Text.Regex.PCRE
+import Text.Regex.TDFA
 
 data Salient
   = Location Text
@@ -20,9 +20,18 @@ data Salient
 
 -- pack is required because PCRE
 salients :: Post Decoded -> [Salient]
-salients p = concatMap (mapMaybe (\(mkTag, regexp) -> mkTag . pack <$> unpack (payload $ text p) =~~ (regexp :: String))) [remoteness, salary, url, email]
+salients p = concatMap (mapMaybe (\(mkTag, regexp) -> mkTag <$> (payload (text p) =~~ (regexp :: String)))) [remoteness, salary, url, email]
   where
     remoteness = [(Location, "(?i)remote *\\(.*?\\)"), (Location, "(?i)remote|hybrid|on-?site")]
     salary = [(Salary, "[\\d,.]*[kK]?[£$€][\\d,.]*[kK]?"), (Salary, "(?i)equity")]
     url = [(URL, "(https?://(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?://(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})")]
-    email = [(Email, "[^@ ]+@[^@ ]+\\.[^@ ]+")]
+    email = [(Email, "[^@ ]+@[^@ .]+\\.[^@ ]+")]
+
+emailRegexp :: String
+emailRegexp = "[^@ ]+@[^@ ]+\\.[^@ ]+"
+
+emailText :: String
+emailText = "You can reach me at [my first name]@slay.cool or submit your application at"
+
+match :: Maybe String
+match = emailText =~~ emailRegexp
