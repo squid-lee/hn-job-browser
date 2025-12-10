3db21bc84e729ca3b11bd58b090de213fcd880d8
Joe Slade <js@complexio.com>
2025-12-10 14:07:36 +0000

    Formatting

--- flake.nix
+++ flake.nix
@@ -2,27 +2,48 @@
   description = "Hacker News - TUI over JSON";
 
   inputs.nixpkgs.url = "nixpkgs";
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f system);
-      nixpkgsFor = forAllSystems (system:
       supportedSystems = [
         "x86_64-linux"
         "aarch64-darwin"
       ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems (
+        system:
         import nixpkgs {
           system = system;
           overlays = [ self.overlays.default ];
-        });
-    in {
-      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt);
-      overlays.default = (final: prev: {
-        dist = final.haskellPackages.callCabal2nix "hnj" ./. { };
-      });
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
+        }
+      );
+    in
+    {
+      formatter = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        pkgs.writeShellApplication {
+          name = "fmt";
+          runtimeInputs = [
+            pkgs.git
+            pkgs.haskellPackages.ormolu
+            pkgs.nixfmt-rfc-style
+          ];
+          text = ''
+            git ls-files -z '*.hs' | xargs -0 ormolu --mode inplace
+            git ls-files -z '*.nix' | xargs -0 nixfmt
+          '';
+        }
+      );
+      overlays.default = (final: prev: { dist = final.haskellPackages.callCabal2nix "hnj" ./. { }; });
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
           default = self.packages.${system}.dist;
           dist = pkgs.dist;
           # checklist-trunc-tables =
@@ -31,12 +52,16 @@
           #               runtimeInputs = [ pkgs.postgresql ];
           #               text = "psql -h 192.168.1.36 -U todo todo -c 'TRUNCATE Lists CASCADE; ALTER SEQUENCE lists_listid_seq RESTART WITH 1; ALTER SEQUENCE listentries_entryid_seq RESTART WITH 1;' ";
           #             };
-        });
+        }
+      );
       checks = self.packages;
 
-      devShells = forAllSystems (system:
-        let haskellPackages = nixpkgsFor.${system}.haskellPackages;
-        in {
+      devShells = forAllSystems (
+        system:
+        let
+          haskellPackages = nixpkgsFor.${system}.haskellPackages;
+        in
+        {
           default = haskellPackages.shellFor {
             packages = p: [ self.packages.${system}.dist ];
             withHoogle = true;
@@ -52,6 +77,7 @@
 
             nativeBuildInputs = [ haskellPackages.hsc2hs ];
           };
-        });
+        }
+      );
     };
 }
